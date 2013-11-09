d704b47962c48d98aa2f314466b35d7e88588698
MrRuiz <rafaelruizmorales@gmail.com>
2013-11-09 16:24:24 +0000

    Changed the branc by using git checkout. Now the new branch is mybranch1

--- index.php
+++ index.php
@@ -1,3 +1,4 @@
 <?php 
-  echo "Hello! from my Site! - branch: master - Repo: test";
+  echo "Hello! from my Site! - Repo: test";
+  echo "Changed the branc by using git checkout. Now the new branch is mybranch1";
 ?>
