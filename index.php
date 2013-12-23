0aa54a0d09f1db3544e912c1955ba30a3ba001fc
Rafael Ruiz <rafaelruizmorales@gmail.com>
2013-12-23 15:49:57 +0000

    Update index.php
    Corrected misspelled word.

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
 <?php 
   echo "Hello! from my Site! - Repo: test";
-  echo "Changed the branc by using git checkout. Now the new branch is mybranch1";
+  echo "Changed the branch by using git checkout. Now the new branch is mybranch1";
 ?>
